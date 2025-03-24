c9b1ba880377964a081e97ddf4a0d8ca6db15370
AwelCarl <axelclaude972@gmail.com>
2025-03-24 14:08:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var markers = L.layerGroup().addTo(map);
+var pointsLayer = L.layerGroup().addTo(map);
+var regionsLayer;
 var dates = [];
 var currentDateIndex = 0;
 var playInterval;
@@ -11,9 +12,9 @@ var speed = 1000;
 var minPower = 0;
 var maxPower = 450;
 var showAllPrevious = false;
+var showPoints = true;
 var colorScale = chroma.scale(['blue', 'yellow', 'red']).domain([0, 450]);
 
-// Create legend control
 var legend = L.control({position: 'topright'});
 legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend');
@@ -80,6 +81,25 @@ fetch('bornes_recharge_mensuel.json')
         };
         timeline.addTo(map);
 
+        fetch('regions-france.geojson')
+            .then(response => response.json())
+            .then(regionsData => {
+                regionsLayer = L.geoJSON(regionsData, {
+                    style: function(feature) {
+                        return {
+                            fillColor: '#FFF',
+                            weight: 2,
+                            opacity: 1,
+                            color: 'white',
+                            fillOpacity: 0.7
+                        };
+                    },
+                    onEachFeature: function(feature, layer) {
+                        layer.bindPopup('');
+                    }
+                }).addTo(map);
+            });
+
         function updateMap(skipInterval) {
             if (currentDateIndex >= dates.length) {
                 clearInterval(playInterval);
@@ -91,26 +111,53 @@ fetch('bornes_recharge_mensuel.json')
 
             document.querySelector('.timeline-input').value = currentDateIndex;
 
-            markers.clearLayers();
+            pointsLayer.clearLayers();
+
+            var regionData = {};
 
             var startIndex = showAllPrevious ? 0 : currentDateIndex;
             for (var i = startIndex; i <= currentDateIndex; i++) {
                 var date = dates[i];
                 data[date].forEach(point => {
                     if (point.puissance_nominale >= minPower && point.puissance_nominale <= maxPower) {
-                        L.circleMarker([point.latitude, point.longitude], {
-                            radius: 5,
-                            fillColor: colorScale(point.puissance_nominale).hex(),
-                            color: "#000",
-                            weight: 1,
-                            opacity: 1,
-                            fillOpacity: 0.8
-                        }).bindPopup(`
-                            <b>Code INSEE:</b> ${point.code_insee}<br>
-                            <b>Nombre de PDC:</b> ${point.nbre_pdc}<br>
-                            <b>Puissance nominale:</b> ${point.puissance_nominale} kW
-                        `).addTo(markers);
+                        if (showPoints) {
+                            L.circleMarker([point.latitude, point.longitude], {
+                                radius: 5,
+                                fillColor: colorScale(point.puissance_nominale).hex(),
+                                color: "#000",
+                                weight: 1,
+                                opacity: 1,
+                                fillOpacity: 0.8
+                            }).bindPopup(`
+                                <b>Code INSEE:</b> ${point.code_insee}<br>
+                                <b>Nombre de PDC:</b> ${point.nbre_pdc}<br>
+                                <b>Puissance nominale:</b> ${point.puissance_nominale} kW
+                            `).addTo(pointsLayer);
+                        }
+
+                        if (!regionData[point.code_insee]) {
+                            regionData[point.code_insee] = {count: 0, totalPower: 0};
+                        }
+                        regionData[point.code_insee].count++;
+                        regionData[point.code_insee].totalPower += point.puissance_nominale;
                     }
+                });
+            }
+
+            if (regionsLayer) {
+                regionsLayer.eachLayer(function(layer) {
+                    var regionCode = layer.feature.properties.code;
+                    var regionStats = regionData[regionCode] || {count: 0, totalPower: 0};
+                    var averagePower = regionStats.count > 0 ? regionStats.totalPower / regionStats.count : 0;
+                    
+                    layer.setStyle({
+                        fillColor: colorScale(averagePower).hex()
+                    });
+                    layer.bindPopup(`
+                        <b>Région:</b> ${layer.feature.properties.nom}<br>
+                        <b>Nombre de bornes:</b> ${regionStats.count}<br>
+                        <b>Puissance moyenne:</b> ${averagePower.toFixed(2)} kW
+                    `);
                 });
             }
 
@@ -142,18 +189,26 @@ fetch('bornes_recharge_mensuel.json')
             minPower = parseInt(this.value);
             document.getElementById('min-power-value').textContent = minPower;
             updateMap(true);
-            updateLegend();
         });
 
         document.getElementById('max-power').addEventListener('input', function() {
             maxPower = parseInt(this.value);
             document.getElementById('max-power-value').textContent = maxPower;
             updateMap(true);
-            updateLegend();
         });
 
         document.getElementById('show-all-previous').addEventListener('change', function() {
             showAllPrevious = this.checked;
+            updateMap(true);
+        });
+
+        document.getElementById('toggle-points').addEventListener('change', function() {
+            showPoints = this.checked;
+            if (showPoints) {
+                map.addLayer(pointsLayer);
+            } else {
+                map.removeLayer(pointsLayer);
+            }
             updateMap(true);
         });
 
